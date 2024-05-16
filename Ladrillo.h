b31aa95b5fc0eb61f9ba30db792925e23922449e
CarolinaMarinHurtado <160075122+CarolinaMarinHurtado@users.noreply.github.com>
2024-05-15 20:14:23 -0500

    Avance

--- Ladrillo.h
+++ Ladrillo.h
@@ -1,4 +1,39 @@
-#ifndef LADRILLO_H
-#define LADRILLO_H
+//#ifndef LADRILLOS_H
+//#define LADRILLOS_H
 
-#endif // LADRILLO_H
+//*#include <QWidget>
+/*#include <QPainter>
+#include <vector>
+
+// Clase para representar un ladrillo
+class ladrillo {
+public:
+    ladrillo(int x, int y, int ancho, int altura, int fuerza);
+
+    int getfuerza() const;
+    void hit();
+
+    QRect rect;
+
+private:
+    int fuerza; // Fuerza del ladrillo
+};
+
+// Clase para representar la ventana del juego
+class ladrillos : public QWidget {
+    Q_OBJECT
+public:
+    ladrillos(QWidget *parent = nullptr);
+
+    void addladrillo(int x, int y, int ancho, int altura, int fuerza);
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    std::vector<ladrillo> ladrillos1; // Vector de ladrillos
+};
+
+#endif // LADRILLOS_H
+
+*/
